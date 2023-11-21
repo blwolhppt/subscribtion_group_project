380d1094c2be40ec2ecff7ceb3cd20a85e54233b
Белова Ольга <OBelova2003@yandex.ru>
2023-11-21 13:58:29 +0300

    start user_model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from .constants import EMAIL_LENGTH, LENGTH
+from .validators import validate_username
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=EMAIL_LENGTH,
+                              unique=True,
+                              blank=False,
+                              null=False,
+                              verbose_name="Почта")
+
+    username = models.CharField(validators=(validate_username,),
+                                max_length=LENGTH,
+                                unique=True,
+                                blank=False,
+                                null=False,
+                                verbose_name='Никнейм')
+
+    image = models.ImageField(verbose_name='Картинка профиля',
+                              upload_to='users/')
+
+    first_name = models.CharField(max_length=LENGTH,
+                                  blank=False,
+                                  null=False,
+                                  verbose_name="Имя")
+
+    last_name = models.CharField(max_length=LENGTH,
+                                 blank=False,
+                                 null=False,
+                                 verbose_name="Фамилия")
+
+    password = models.CharField(max_length=LENGTH,
+                                verbose_name="Пароль")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
+
+    def __str__(self):
+        return self.username
